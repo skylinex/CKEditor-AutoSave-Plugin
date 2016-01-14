0b700bc3257fcc6ec33151f27ac573de2bc84011
Oldřich Válek <oldrich.valek@gmail.com>
2016-01-14 18:34:48 +0100

    Update cs.js
    Typing error...

--- autosave/lang/cs.js
+++ autosave/lang/cs.js
@@ -5,7 +5,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.plugins.setLang('autosave', 'cs', {
     dateFormat: 'LLL',
     autoSaveMessage: 'Automaticky uloženo',
-    loadSavedContent: 'byla nalezena automaticky uložená verze obsahu ze dne "{0}". Chcete porovnat obsah a zvolit si s kterým obsahem chete pracovat?',
+    loadSavedContent: 'byla nalezena automaticky uložená verze obsahu ze dne "{0}". Chcete porovnat obsah a zvolit si s kterým obsahem chcete pracovat?',
     title: 'Porovnejre automaticky uložený obsah s obsahem nahraným z webové stránky',
     loadedContent: 'Nahraný obsah',
     autoSavedContent: 'Automaticky uložený obsah z: \'',
